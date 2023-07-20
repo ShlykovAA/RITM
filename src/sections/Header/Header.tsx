a8c9b015ba60c61d037b45155c56bf2521c0f16c
ShlykovAA <anatolio7z@gmail.com>
2023-07-20 16:10:26 +0300

    add telegram

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -4,7 +4,9 @@ import { useMatchMedia } from '../../hooks';
 import styles from './Header.module.scss';
 import cn from 'classnames';
 import imgLang from '/lang.png';
+import telegram from '/telegram-240.png';
 import { NavDesktop } from '../../components/Nav-desktop';
+import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
   const { isMobile, isTablet }:any = useMatchMedia();
@@ -12,9 +14,13 @@ export const Header = () => {
   console.log('isMobile and tablet', isMobile || isTablet )
   return (
     <header className={styles.header}>
-      <img src="/RITM-logo-1-1489.png" alt="Ritm logo" className={styles.logo}/>
+      <NavLink to="/" className={styles.logo_link}>
+        <img src="/RITM-logo-1-1489.png" alt="Ritm logo" className={styles.logo}/>
+      </NavLink>
+
       { isMobile || isTablet ? <NavMobile isOpen={isOpen} /> : <NavDesktop />}
       <div className={styles.lang}>
+        <img src={telegram} alt="telegram" className={styles.lang_img}/>
         <img src={imgLang} alt="lang planet" className={styles.lang_img} />
         <span className={styles.span_lang}>UA</span>
         <span className={styles.span_slash}>/</span>
