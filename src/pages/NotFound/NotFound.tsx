f893273939e02ccf9fb4e11f209d245fa595abdb
ShlykovAA <anatolio7z@gmail.com>
2023-12-21 17:07:36 +0200

    One page for 404

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const NotFound = () => {
   return (
     <main className='main'>
-      <h1>Sorry, page not found. You should come back to <Link to="/">Home</Link></h1>
+      <h1>Sorry, page not found. At the moment only one page is ready <Link to="/vacancies">Вакансії</Link></h1>
     </main>
   );
 };
