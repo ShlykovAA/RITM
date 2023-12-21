4b5b6741442615e464b8ae4269b8e9997ca7e69f
ShlykovAA <anatolio7z@gmail.com>
2023-12-21 17:09:31 +0200

    One page for 404 v2

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const Home = () => {
   return (
     <main className='main'>
       <h1>Home page</h1>
+      <h1>At the moment only one page is ready <Link to="/vacancies">Вакансії</Link></h1>
     </main>
   );
 };
